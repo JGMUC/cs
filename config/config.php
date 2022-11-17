5d2d6fcd1d11ddcb9d39dd04e975a6ae47777b74
Javier Garay <javier.garay@ALFAGL.COM>
2022-11-17 16:01:34 -0500

    Deploy configurado

--- config/config.php
+++ config/config.php
@@ -1,9 +1,8 @@
 <?php
-define('URL', 'https://cssoftware.herokuapp.com/');
+define('URL', 'https://cssoftware.herokuapp.com/cs/');
 define('HOST', 'us-cdbr-east-06.cleardb.net');
 define('DB', 'heroku_9ce58dea0c0337f');
 define('USER', 'b4c59a630144c5');
 define('PASSWORD', 'e50b9e5d');
 define('CHARSET', 'utf8mb4');
-
 ?>
